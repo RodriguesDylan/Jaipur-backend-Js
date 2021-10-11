467e7842dc3c5a53aa4986773d9b561854960868
RodriguesDylan <dylan.rodriguestop@gmail.com>
2021-10-11 18:00:56 +0200

    correction des tests

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express"
 
 import healthRouter from "./healthRouter"
+import gameRouter from "./gameRouter"
 // Don't forget to import new routers above
 
 const router = express.Router()
 
 router.use("/health", healthRouter)
-router.use("/game", gameRouter)
+router.use("/games", gameRouter)
 // Add new routers above
 
 export default router
