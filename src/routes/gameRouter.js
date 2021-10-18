51d43286a8cc4b80ebdb868210871d26a464a39d
RodriguesDylan <dylan.rodriguestop@gmail.com>
2021-10-18 18:30:53 +0200

    ajout du PUT /game/[id]/take-good V1.0

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -12,4 +12,19 @@ router.post("/", function (req, res) {
   res.status(201).json(newGame)
 })
 
+// Listen to PUT /game/[id]/take-good
+router.put("/games/" + newGame.game.id + "/take-good", function (req, res) {
+  if (!req.path.id) {
+    return res.status(400).send("Missing id parameter")
+  }
+  if (!req.header.playerIndex) {
+    return res.status(400).send("Missing playerIndex parameter")
+  }
+  if (!req.body.takeGoodPayload) {
+    return res.status(400).send("Missing good parameter")
+  }
+  takeGood(req.path.id, req.header.playerIndex, req.body.takeGoodPayload)
+  res.status(200).send("OK")
+})
+
 export default router
